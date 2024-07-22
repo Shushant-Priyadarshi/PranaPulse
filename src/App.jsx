c500990dd63d5e31d6abaf1f56605c774d2209ab
Shushant-Priyadarshi <profshushant@gmail.com>
2024-07-22 19:21:05 +0530

    landing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
+import Front from "./components/Body/Front";
+import NavBar from "./components/header/NavBar";
+
 function App() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <NavBar/>
+      <Front/>
     </>
   );
 }
